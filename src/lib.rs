385ceae05196df400d5eb84993e99aeace5a5ede
Steven Dwy <yoplitein+git@gmail.com>
2026-02-02 18:28:50 -0800

    Add helpers to parse and compile

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,21 @@ pub mod compiler;
 pub mod lexer;
 pub mod parser;
 
+use crate::parser::Module;
+
 pub type AResult<T> = anyhow::Result<T>;
+
+pub fn parse_module(code: &str) -> AResult<Module> {
+	let tokens = self::lexer::lex(code)?;
+	let mut module = self::parser::parse(&tokens)?;
+	module.propagate_constants();
+	module.fold_constants();
+	Ok(module)
+}
+
+pub fn compile<Compiler: self::compiler::Compiler + Default>(
+	module: &Module,
+) -> AResult<Compiler::Output> {
+	let mut compiler: Compiler = Default::default();
+	compiler.compile_module(module)
+}
