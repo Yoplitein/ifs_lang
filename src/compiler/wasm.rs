df322d661510c0bb2c1b619dbcbb00c637e03ded
Steven Dwy <yoplitein+git@gmail.com>
2026-03-17 18:18:23 -0700

    Rewrite WASM compiler to emit WAT instead
    Much less headache trying to follow indices, and also massively simplfiies implementation of intrinsics (e.g. no having to translate data structures between `wasmparser` -> `wasm-encoder`)

--- src/compiler/wasm.rs
+++ src/compiler/wasm.rs
@@ -1,62 +1,32 @@
-use std::collections::HashMap;
+use std::{
+	collections::{HashMap, HashSet},
+	fmt::Write,
+};
 
-use anyhow::Ok;
+use anyhow::{Ok, bail, ensure};
 use num::Complex;
 
 use super::Compiler;
 use crate::{
 	AResult,
 	lexer::Value,
-	parser::{Expr, Module},
+	parser::{ArgumentSet, Expr, Module},
 };
 
+#[derive(Default)]
 pub struct WasmCompiler {
-	global_indices: HashMap<String, u32>,
-	argument_indices: HashMap<String, u32>,
-	current_function: wasm_encoder::Function,
-	// name => (arity, index)
-	host_functions: HashMap<String, (usize, u32)>,
-}
-
-impl Default for WasmCompiler {
-	fn default() -> Self {
-		Self {
-			global_indices: HashMap::new(),
-			argument_indices: HashMap::new(),
-			current_function: wasm_encoder::Function::new(vec![]),
-			host_functions: HashMap::new(),
-		}
-	}
+	globals: HashSet<String>,
+	arguments: ArgumentSet,
+	current_function: String,
+	// name => arity
+	host_functions: HashMap<String, usize>,
 }
 
 pub struct WasmOutput {
-	pub global_indices: HashMap<String, u32>,
-	pub argument_indices: HashMap<String, u32>,
+	pub globals: HashSet<String>,
+	pub arguments: ArgumentSet,
 	pub num_ifs_functions: usize,
-	pub wasm_module: Vec<u8>,
-}
-
-#[derive(Default)]
-struct WasmModule {
-	types: wasm_encoder::TypeSection,
-	imports: wasm_encoder::ImportSection,
-	functions: wasm_encoder::FunctionSection,
-	globals: wasm_encoder::GlobalSection,
-	exports: wasm_encoder::ExportSection,
-	code: wasm_encoder::CodeSection,
-}
-
-impl WasmModule {
-	fn finish(self) -> Vec<u8> {
-		let mut module = wasm_encoder::Module::new();
-		module.section(&self.types);
-		module.section(&self.imports);
-		module.section(&self.functions);
-		module.section(&self.globals);
-		module.section(&self.exports);
-		module.section(&self.code);
-		module.finish()
-	}
+	pub wat_module: String,
 }
 
 impl Compiler for WasmCompiler {
@@ -64,138 +34,93 @@ impl Compiler for WasmCompiler {
 	type Output = WasmOutput;
 
 	fn compile_module(mut self, module: &Module) -> AResult<Self::Output> {
-		let mut wasm_module = WasmModule::default();
+		self.globals.clone_from(&module.globals);
+		self.arguments.clone_from(&module.arguments);
 
-		// declare and export globals
-		for (index, name) in module.globals.iter().enumerate() {
-			let global_index = wasm_module.globals.len();
-			wasm_module.globals.global(
-				wasm_encoder::GlobalType {
-					val_type: wasm_encoder::ValType::V128,
-					mutable: true,
-					shared: true,
-				},
-				&wasm_encoder::ConstExpr::v128_const(1),
-			);
-			wasm_module.exports.export(
-				&format!("v{index}"),
-				wasm_encoder::ExportKind::Global,
-				global_index,
-			);
-			let None = self.global_indices.insert(name.clone(), global_index) else {
-				anyhow::bail!("global {name:?} defined more than once")
-			};
+		let mut globals = Vec::with_capacity(self.globals.len());
+		for name in &self.globals {
+			globals.push(format!(
+				"(global ${name} (export \"{name}\") (mut v128) (v128.const f64x2 0.0 0.0))\n"
+			))
 		}
 
-		// declare IFS function type
-		let ifs_func_type = wasm_module.types.len();
-		wasm_module.types.ty().function(
-			(0 .. module.arguments.len()).map(|_| wasm_encoder::ValType::V128),
-			[wasm_encoder::ValType::V128],
-		);
-
-		// define argument (local) indices
-		for (index, name) in module.arguments.iter().enumerate() {
-			let None = self.argument_indices.insert(name.clone(), index as _) else {
-				anyhow::bail!("argument {name:?} defined more than once");
-			};
-		}
-
-		// function codegen
-		for func in &module.functions {
-			self.compile_node(&func.body)?;
-			let mut compiled_func = std::mem::replace(
-				&mut self.current_function,
-				wasm_encoder::Function::new(vec![]),
-			);
-			compiled_func.instructions().end();
-			let code_index = wasm_module.code.len();
-			wasm_module.code.function(&compiled_func);
-			let func_index = wasm_module.functions.len();
-			assert_eq!(code_index, func_index);
-			wasm_module.functions.function(ifs_func_type);
-		}
-
-		// declare imports
-		let mut host_functions: Vec<_> = self
-			.host_functions
-			.into_iter()
-			.map(|(name, (arity, index))| (index, name, arity))
-			.collect();
-		host_functions.sort_by_key(|(index, ..)| *index);
-		let num_host_functions = host_functions.len();
-		let mut host_function_types = HashMap::new();
-		#[cfg(debug_assertions)]
-		let mut last_index = 0;
-		for (index, name, arity) in host_functions {
-			#[cfg(debug_assertions)]
-			{
-				match (last_index, index) {
-					(0, 0) => {},
-					(a, b) if b == a + 1 => {},
-					_ => panic!("host functions have non-monotonic indices"),
+		let args = {
+			let mut ordered = Vec::from_iter(self.arguments.names());
+			ordered.sort_by_key(|name| self.arguments.get(name));
+			let mut args = String::new();
+			for name in ordered {
+				if !args.is_empty() {
+					args.push(' ');
 				}
-				last_index = index;
+				args.write_fmt(format_args!("(param ${name} v128)"))?;
 			}
+			args
+		};
+		let mut functions = Vec::with_capacity(module.functions.len());
+		for (index, function) in module.functions.iter().enumerate() {
+			self.compile_node(&function.body)?;
+			let instructions = std::mem::take(&mut self.current_function);
+			functions.push(format!(
+				"(func (export \"f{index}\") {args} (result v128)\n{instructions})\n"
+			));
+		}
 
-			let func_ty = *host_function_types.entry(arity).or_insert_with(|| {
-				let ty_index = wasm_module.types.len();
-				wasm_module
-					.types
-					.ty()
-					.function((0 .. arity).map(|_| wasm_encoder::ValType::V128), [
-						wasm_encoder::ValType::V128,
-					]);
-				ty_index
+		let mut imports = Vec::with_capacity(self.host_functions.len());
+		let mut arg_decls = HashMap::new();
+		for (name, &arity) in &self.host_functions {
+			let args = &*arg_decls.entry(arity).or_insert_with(|| {
+				let mut args = String::new();
+				for _ in 0 .. arity {
+					if !args.is_empty() {
+						args.push(' ');
+					}
+					args.push_str("(param v128)");
+				}
+				args
 			});
-			wasm_module
-				.imports
-				.import("host", &name, wasm_encoder::EntityType::Function(func_ty));
+			imports.push(format!(
+				"(import \"host\" \"{name}\" (func $host_{name} {args} (result v128)))\n"
+			));
 		}
 
-		// export IFS functions
-		for index in 0 .. module.functions.len() {
-			wasm_module.exports.export(
-				&format!("f{index}"),
-				wasm_encoder::ExportKind::Func,
-				(num_host_functions + index) as u32,
-			);
+		let mut wat_module = String::from("(module\n");
+		for item in imports.into_iter().chain(globals).chain(functions) {
+			wat_module.write_str(&item)?;
 		}
+		wat_module.write_str(")")?;
 
-		let global_indices = self.global_indices;
-		let argument_indices = self.argument_indices;
+		let globals = self.globals;
+		let arguments = self.arguments;
 		let num_ifs_functions = module.functions.len();
-		let wasm_module = wasm_module.finish();
 		let output = WasmOutput {
-			global_indices,
-			argument_indices,
+			globals,
+			arguments,
 			num_ifs_functions,
-			wasm_module,
+			wat_module,
 		};
 		Ok(output)
 	}
 
 	fn compile_node(&mut self, expr: &Expr) -> AResult<Self::NodeOutput> {
 		match expr {
-			Expr::Constant(value) => {
-				let literal = match value {
-					Value::Real(val) => val.to_bits() as _,
-					Value::Complex(val) => {
-						let Complex { re, im } = val;
-						let (re, im) = (re.to_bits() as u128, im.to_bits() as u128);
-						(im << 64 | re) as _
-					},
+			&Expr::Constant(value) => {
+				let (re, im) = match value {
+					Value::Real(val) => (val, 0.0),
+					Value::Complex(Complex { re, im }) => (re, im),
 				};
-				self.current_function.instructions().v128_const(literal);
+				self.current_function
+					.write_fmt(format_args!("v128.const f64x2 {re} {im}\n"))?;
 			},
 			Expr::Variable(name) => {
-				if let Some(&index) = self.global_indices.get(name) {
-					self.current_function.instructions().global_get(index);
-				} else if let Some(&index) = self.argument_indices.get(name) {
-					self.current_function.instructions().local_get(index);
+				if self.globals.contains(name) {
+					self.current_function
+						.write_fmt(format_args!("global.get ${name}\n"))?;
+				} else if self.arguments.contains(name) {
+					self.current_function
+						.write_fmt(format_args!("local.get ${name}\n"))?;
 				} else {
-					anyhow::bail!("trying to read undefined variable {name:?}")
-				};
+					bail!("trying to read undefined variable {name:?}")
+				}
 			},
 			Expr::Add(l, r) |
 			Expr::Sub(l, r) |
@@ -206,10 +131,10 @@ impl Compiler for WasmCompiler {
 				self.compile_node(&r)?;
 				match expr {
 					Expr::Add(..) => {
-						self.current_function.instructions().f64x2_add();
+						self.current_function.write_str("f64x2.add\n")?;
 					},
 					Expr::Sub(..) => {
-						self.current_function.instructions().f64x2_sub();
+						self.current_function.write_str("f64x2.sub\n")?;
 					},
 					Expr::Mul(..) => {
 						todo!()
@@ -225,7 +150,7 @@ impl Compiler for WasmCompiler {
 			},
 			Expr::Neg(expr) => {
 				self.compile_node(expr)?;
-				self.current_function.instructions().f64x2_neg();
+				self.current_function.write_str("f64x2.neg\n")?;
 			},
 			Expr::Call { function, args } => {
 				let arity = args.len();
@@ -234,12 +159,15 @@ impl Compiler for WasmCompiler {
 				}
 
 				// TODO: intrinsics
-				let index = self.host_functions.len() as u32;
-				let &mut (_, index) = self
-					.host_functions
-					.entry(function.clone())
-					.or_insert((arity, index));
-				self.current_function.instructions().call(index);
+				if let Some(last_arity) = self.host_functions.insert(function.clone(), arity) {
+					ensure!(
+						arity == last_arity,
+						"host function {function:?} has disallowed varying arity ({last_arity} / \
+						 {arity})"
+					);
+				}
+				self.current_function
+					.write_fmt(format_args!("call $host_{function}\n"))?;
 			},
 		}
 		Ok(())
